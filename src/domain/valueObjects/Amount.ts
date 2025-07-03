6a83104684aea4106c72075affc98d657d6f096a
Rafael de Almeida <rafael_allx@hotmail.com>
2025-07-03 11:17:25 -0300

    Create Amount.ts

---
+++ src/domain/valueObjects/Amount.ts
@@ -0,0 +1,14 @@
+// src/domain/valueObjects/Amount.ts
+export class Amount {
+  constructor(private readonly value: number) {
+    if (value <= 0) throw new Error('Valor inválido');
+  }
+
+  getValue(): number {
+    return this.value;
+  }
+
+  isGreaterThan(limit: number): boolean {
+    return this.value > limit;
+  }
+}
